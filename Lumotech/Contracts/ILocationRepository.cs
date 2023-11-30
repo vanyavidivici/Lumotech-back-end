f1839a185b02753103bfece02d5c808838d2ed5f
vanyavidivici <vanyavidivici@gmail.com>
2023-11-30 18:14:59 +0200

    added repository layer for location entity

--- Lumotech/Contracts/ILocationRepository.cs
+++ Lumotech/Contracts/ILocationRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Contracts;
+﻿using Entities.Models;
+
+namespace Contracts;
 
 public interface ILocationRepository
 {
-    
+    Task<IEnumerable<Location>> GetAllLocationsAsync(bool trackChanges);
+    Task<Location> GetLocationAsync(Guid locationId, bool trackChanges);
+    void CreateLocation(Location location);
+    void DeleteLocation(Location location);
 }
--- Lumotech/Repository/LocationRepository.cs
+++ Lumotech/Repository/LocationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
@@ -8,4 +9,17 @@ public class LocationRepository : RepositoryBase<Location>, ILocationRepository
     public LocationRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
     }
+
+    public async Task<IEnumerable<Location>> GetAllLocationsAsync(bool trackChanges) =>
+        await FindAll(trackChanges)
+            .OrderBy(c => c.Country)
+            .ToListAsync();
+    
+    public async Task<Location> GetLocationAsync(Guid locationId, bool trackChanges) =>
+        await FindByCondition(c => c.Id.Equals(locationId), trackChanges)
+            .SingleOrDefaultAsync();
+
+    public void CreateLocation(Location location) => Create(location);
+
+    public void DeleteLocation(Location location) => Delete(location);
 }
