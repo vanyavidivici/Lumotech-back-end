03ce9233569d2c68447aed95dd4ad039e8f7dbc4
vanyavidivici <vanyavidivici@gmail.com>
2023-12-06 23:41:08 +0200

    added authorization support to swagger

--- Lumotech/Lumotech/Extensions/ServiceExtensions.cs
+++ Lumotech/Lumotech/Extensions/ServiceExtensions.cs
@@ -70,6 +70,31 @@ public static class ServiceExtensions
         {
             s.SwaggerDoc("v1", new OpenApiInfo { Title = "Lumotech API", Version = "v1"
             });
+            
+            s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Place to add JWT with Bearer",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            
+            s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Name = "Bearer",
+                    },
+                    new List<string>()
+                }
+            });
         });
     }
     
